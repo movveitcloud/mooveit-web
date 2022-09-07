11ae8389a6eeda081b9c981422c52bb64d8860e7
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-09-07 11:55:28 +0100

    folder updates

---
+++ pages/search/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = () => {
+  return <div>index</div>;
+};
+
+export default Search;
---
+++ pages/signin/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>index</div>;
+};
+
+export default SignIn;
