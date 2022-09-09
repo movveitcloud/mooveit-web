f79bf3f94e839a1039bbd20ab9b23c5fd77444a1
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-09-09 09:19:51 +0100

    Added email validation to email fields and design verification pages

--- components/shared/FormInput.jsx
+++ components/shared/FormInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FormInput = ({ label, type, name, disabled, register, errors, errorMessage }) => {
+const FormInput = ({ label, type, name, register, errors, errorMessage }) => {
   return (
     <div className="flex flex-col mb-4 w-full">
       <label className="text-base text-gray-800 mb-2 inline-block text-left">{label}</label>
@@ -16,7 +16,6 @@ const FormInput = ({ label, type, name, disabled, register, errors, errorMessage
             message: "Please enter a valid email address",
           },
         })}
-        disabled={disabled}
       />
       {name == "email" && errors.email?.message
         ? ""
