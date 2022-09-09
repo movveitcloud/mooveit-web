edb305324149e2572539987960b9a474e8098342
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-09-09 10:26:04 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { HowItWorks, Why, FindStorage, HeroSection } from "../components/home";
-import FeaturedListings from "../components/shared/FeaturedListings";
 import { PageLayout } from "../components/layouts";
 import { FeaturedListings } from "../components/shared";
 
