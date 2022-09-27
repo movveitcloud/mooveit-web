7d69ae54de83ae01ddd88b7edfea263070175e17
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-09-27 17:36:49 +0100

    preview btn

--- components/listings/Steppers.jsx
+++ components/listings/Steppers.jsx
@@ -8,7 +8,9 @@ const Steppers = ({ activeStepper, setActiveStepper }) => {
           <li
             key={i}
             data-content={i < activeStepper ? "✓" : i + 1}
-            className={`${i <= activeStepper ? "step-accent text-primary" : "text-[#959595]"} step cursor-pointer`}
+            className={`${
+              i <= activeStepper ? "step-accent text-primary" : "step-info text-[#959595]"
+            } step cursor-pointer`}
             onClick={() => setActiveStepper(i)}>
             {item}
           </li>
--- pages/listings/create.js
+++ pages/listings/create.js
@@ -15,23 +15,28 @@ const NewListing = () => {
 
   return (
     <NewListingLayout>
-      <div className="relative">
-        <div className="w-[60%] mx-auto relative py-5">
+      <div className="">
+        <div className="py-5">
           <div className="sticky top-0 bg-[#F9F9F9]">
-            <Steppers activeStepper={activeStepper} setActiveStepper={setActiveStepper} />
+            <div className="w-[60%] mx-auto">
+              <Steppers activeStepper={activeStepper} setActiveStepper={setActiveStepper} />
+            </div>
+            <div className="absolute right-10 top-5 border border-primary text-primary text-sm hover:bg-primary hover:text-white p-4 rounded-md cursor-pointer">
+              <EyeIcon className="w-4 mx-auto" />
+              <p>Preview</p>
+            </div>
           </div>
-          {/* <div className="absolute right-0 border p-8 rounded-md">
-            <EyeIcon className="w-4" />
-            <p>Preview</p>
-          </div> */}
-          <div>
-            {activeStepper == 0 && <BasicInformationStepper />}
-            {activeStepper == 1 && <SpaceDetailsStepper />}
-            {activeStepper == 2 && <AvailabilityStepper />}
-            {activeStepper == 3 && <PricingStepper />}
-          </div>
-          <div className="my-16">
-            <StepperControls activeStepper={activeStepper} setActveStepper={setActiveStepper} />
+
+          <div className="w-[60%] mx-auto">
+            <div>
+              {activeStepper == 0 && <BasicInformationStepper />}
+              {activeStepper == 1 && <SpaceDetailsStepper />}
+              {activeStepper == 2 && <AvailabilityStepper />}
+              {activeStepper == 3 && <PricingStepper />}
+            </div>
+            <div className="my-16">
+              <StepperControls activeStepper={activeStepper} setActveStepper={setActiveStepper} />
+            </div>
           </div>
         </div>
       </div>
