971f466d45c397a384d3c5f56db7b8537f4d56d2
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-10-11 11:26:29 +0100

    update

--- components/modals/PreviewModal.jsx
+++ components/modals/PreviewModal.jsx
@@ -6,7 +6,7 @@ const PreviewModal = () => {
   return (
     <>
       <input type="checkbox" id="preview" className="modal-toggle" />
-      <label htmlFor="preview" className="modal">
+      <label htmlFor="previe" className="modal">
         <label className="modal-box p-8 relative w-[80%] md:w-[60%] max-w-[600px] rounded-xl z-20 bg-[#F9F9F9]">
           <h2 className="font-semibold text-xl text-primary">You're Almost There!</h2>
           <label
@@ -17,10 +17,7 @@ const PreviewModal = () => {
           <div className="my-8 flex justify-center">
             <ListingCard />
           </div>
-          <div className="space-y-4">
-            <button className="btn btn-outline btn-primary hover:btn-accent w-full font-normal">Preview</button>
-            <button className="btn btn-primary w-full font-normal">Publish Listing</button>
-          </div>
+          <button className="btn btn-primary w-full font-normal">Publish Listing</button>
         </label>
       </label>
     </>
