ccf14b314c6a8cecb8d36a95b3eab0049b500c8c
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-10-13 00:00:01 +0100

    sticky update

--- pages/book/[listingId]/index.js
+++ pages/book/[listingId]/index.js
@@ -5,15 +5,18 @@ import BookListingMainCard from "../../../components/book-listing/BookListingMai
 const BookListing = () => {
   return (
     <PageLayout>
-      <div className="max-w-[90%] lg:max-w-[85%] mx-auto py-3 md:py-5 pb-12">
-        <div className="flex flex-col lg:flex-row space-y-6 lg:space-x-8 lg:space-y-0">
+      <div className="max-w-[90%] lg:max-w-[85%] mx-auto py-3 md:py-5 ">
+        <div className="flex flex-col lg:flex-row space-y-6 lg:space-x-8 lg:space-y-0 pb-12">
           <div className="w-full lg:w-[60%] flex flex-col space-y-8">
+            <BookListingMainCard />
             <BookListingMainCard />
           </div>
 
-          <div className="w-full lg:w-[40%] flex flex-col md:flex-row lg:flex-col space-y-6 space-x-0 lg:space-y-8 md:space-y-0 md:space-x-8 lg:space-x-0">
-            <BookNow />
-            <MessagePartner />
+          <div className="w-full lg:w-[40%]">
+            <div className="lg:sticky lg:top-8 flex flex-col md:flex-row lg:flex-col space-y-6 space-x-0 lg:space-y-8 md:space-y-0 md:space-x-8 lg:space-x-0">
+              <BookNow />
+              <MessagePartner />
+            </div>
           </div>
         </div>
       </div>
