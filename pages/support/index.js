61d4a8bfc73bbb10a0ec1824f7c352db3569f2e2
profnike <profnike@gmail.com>
2022-10-12 18:26:28 +0100

    cleaning up of unused code

--- pages/support/index.js
+++ pages/support/index.js
@@ -4,11 +4,7 @@ import { Landing, PageLayout,Location, SimilarListings } from "../../components"
 
 const Support = () => {
   return <PageLayout>Support
-    <Landing/>
-    <div className="h-4 w-full "></div>
-    <Location/>
-    <div className="h-4 w-full "></div>
-    <SimilarListings/>
+   
   </PageLayout>;
 };
 
