b197070c19c9b75d162f9c613d55028c7d8bc351
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-10-12 23:47:03 +0100

    booking page main, book and partner components

---
+++ pages/book/[listingId]/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BookNow, MessagePartner, PageLayout } from "../../../components";
+import BookListingMainCard from "../../../components/book-listing/BookListingMainCard";
+
+const BookListing = () => {
+  return (
+    <PageLayout>
+      <div className="max-w-[90%] lg:max-w-[85%] mx-auto py-3 md:py-5 pb-12">
+        <div className="flex flex-col lg:flex-row space-y-6 lg:space-x-8 lg:space-y-0">
+          <div className="w-full lg:w-[60%] flex flex-col space-y-8">
+            <BookListingMainCard />
+          </div>
+
+          <div className="w-full lg:w-[40%] flex flex-col md:flex-row lg:flex-col space-y-6 space-x-0 lg:space-y-8 md:space-y-0 md:space-x-8 lg:space-x-0">
+            <BookNow />
+            <MessagePartner />
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default BookListing;
