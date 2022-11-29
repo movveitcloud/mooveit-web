57c164acca64b377dd444dfed52202dcf0143dae
oghenero <sigbenuoghenero@gmail.com>
2022-11-29 17:13:51 +0100

    updates

--- components/listings/Media.jsx
+++ components/listings/Media.jsx
@@ -5,10 +5,13 @@ import { UploadIcon, XIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { ListingInputContext } from "../../context";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import { toast } from "react-toastify";
 
 const Media = ({ edit, id, incomplete }) => {
   const { formDetails, setFormDetails } = useContext(ListingInputContext);
   const [preview, setPreview] = useState([]);
+  const [uploading, setUploading] = useState(false);
   const [, refresh] = useState();
   const fileRef = useRef(null);
   const { data } = useSelector((state) => state.listing);
@@ -23,28 +26,42 @@ const Media = ({ edit, id, incomplete }) => {
 
   const onSelectFile = async (e) => {
     let files = [];
+    setUploading(true);
     files = [...files, e.target.files];
     files?.forEach(async (file, i) => {
-      const formData = new FormData();
-      if (formData) {
-        formData.append("id", listingId);
-        formData.append("key", "media");
-        formData.append("media", file[i]);
-      }
+      if (file[i].size > 5031070) {
+        toast.error("Please upload a file smaller than 5 MB");
+        setUploading(false);
+      } else {
+        toast.success("File uploaded successfully");
 
-      try {
-        const headers = {
-          Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
-          "Content-Type": "multipart/formdata",
-        };
-        const response = await API({
-          method: "patch",
-          url: `/listings/${listingId}/upload`,
-          headers: headers,
-          data: formData,
-        });
-        setFormDetails({ ...formDetails, image: response.data.data });
-      } catch (error) {}
+        const formData = new FormData();
+        if (formData) {
+          formData.append("id", listingId);
+          formData.append("key", "media");
+          formData.append("media", file[i]);
+        }
+
+        try {
+          const headers = {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
+            "Content-Type": "multipart/formdata",
+          };
+          setUploading(true);
+
+          const response = await API({
+            method: "patch",
+            url: `/listings/${listingId}/upload`,
+            headers: headers,
+            data: formData,
+          });
+
+          setFormDetails({ ...formDetails, image: response.data.data });
+          setUploading(false);
+        } catch (error) {
+          setUploading(false);
+        }
+      }
     });
   };
 
@@ -76,23 +93,46 @@ const Media = ({ edit, id, incomplete }) => {
           <div className="flex flex-wrap">
             {formDetails?.image?.map((img, index) => (
               <div key={index} className="w-full relative md:w-[32%] h-[200px] mb-[2.5rem] rounded-md mr-2">
-                <img
-                  src={img}
-                  id={index}
-                  alt="pic1"
-                  name="media"
-                  className="w-[95%] h-full mb-2 object-cover rounded"
-                />
-                <span className="bg-black p-2  hover:bg-red-500 cursor-pointer  rounded-[50%] absolute  -right-0  -top-2 ">
-                  <XIcon
-                    id={index}
-                    key={index}
-                    className="text-white w-4  font-bold"
-                    onClick={(e) => {
-                      removeItem(e, index);
-                    }}
-                  />
-                </span>
+                {uploading ? (
+                  <div>
+                    <Skeleton height={225} />
+                  </div>
+                ) : img.substring(img.lastIndexOf(".") + 1).toLowerCase() === "mp4" ||
+                  img.substring(img.lastIndexOf(".") + 1).toLowerCase() === "mov" ? (
+                  <>
+                    <video src={img} controls className="w-[95%] h-full mb-2 object-cover rounded"></video>
+                    <span className="bg-black p-2  hover:bg-red-500 cursor-pointer  rounded-[50%] absolute  -right-0  -top-2 ">
+                      <XIcon
+                        id={index}
+                        key={index}
+                        className="text-white w-4  font-bold"
+                        onClick={(e) => {
+                          removeItem(e, index);
+                        }}
+                      />
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <img
+                      src={img}
+                      id={index}
+                      alt="pic1"
+                      name="media"
+                      className="w-[95%] h-full mb-2 object-cover rounded"
+                    />
+                    <span className="bg-black p-2  hover:bg-red-500 cursor-pointer  rounded-[50%] absolute  -right-0  -top-2 ">
+                      <XIcon
+                        id={index}
+                        key={index}
+                        className="text-white w-4  font-bold"
+                        onClick={(e) => {
+                          removeItem(e, index);
+                        }}
+                      />
+                    </span>
+                  </>
+                )}
               </div>
             ))}
             {formDetails?.image?.length > 0 && (
@@ -114,7 +154,14 @@ const Media = ({ edit, id, incomplete }) => {
             </button>
           )}
           <div className="text-center hidden">
-            <input accept="image/*" type="file" multiple name="file" onChange={onSelectFile} ref={fileRef} />
+            <input
+              accept="video/mp4,video/x-m4v,video/mov,video/*,image/jpg, image/png, image/*"
+              type="file"
+              multiple
+              name="file"
+              onChange={onSelectFile}
+              ref={fileRef}
+            />
           </div>
         </div>
       </div>
